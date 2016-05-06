1a42abefc14f86f6be57f990282b39501506324b
Bryan Alexis Mejia Fonseca <bryamejia@hotmail.com>
2016-05-06 18:28:21 -0500

    Funciona Diagonales

--- Main.cpp
+++ Main.cpp
@@ -44,6 +44,8 @@ int main(int argc, char*argv[])
 					cout << "Jugada invalida" << endl;
 				}
 			}
+			int movimientoSexy;
+			int a,b;
 		//Juga Turno 1
 			for (int i = 5; i >= 0; --i)
 			{
@@ -51,7 +53,27 @@ int main(int argc, char*argv[])
 				{
 					matri[i][col]=1;
 					i=-1;
+					a = i;
+					b = col;
+					movimientoSexy = matri[i][col];
 				}
+			}
+
+			//Verificacion de Jugador 1;
+			int vertical = 1;
+			int horizontal = 1;
+			int diagonal1 = 1;
+			int diagonal2 = 1;
+			for (int i = a + 1; matri[i][b] == movimientoSexy && i <= 5; i++){
+				vertical++;
+			}
+			for (int i = a -1; matri[i][b] == movimientoSexy && i >= 0; i--){
+				vertical++;
+			}
+			if (vertical >= 4)
+			{
+				cout << "Gana jugador 1." << endl;
+				gane = true;
 			}
 
 		//Imprime Turno 1
@@ -83,17 +105,17 @@ int main(int argc, char*argv[])
 					cout << "Jugada invalida" << endl;
 				}
 			}
-			//Juga
+			//Juga 
 			for (int i = 5; i >= 0; --i)
 			{
-				if (matri[i][col]==0)
+				if (matri[i][col2]==0)
 				{
-					matri[i][col]=2;
+					matri[i][col2]=2;
 					i=-1;
 				}
 			}
 
-			//Imprime
+			//Imprime Jugada del 2
 			for (int i = 0; i < 7; ++i)
 			{
 				cout << i << "\t";
