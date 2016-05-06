85402d02953a8a3233e29ddfd8dce7157329d3de
Bryan Alexis Mejia Fonseca <bryamejia@hotmail.com>
2016-05-06 16:19:49 -0500

    Funciona Jugada 1

--- Main.cpp
+++ Main.cpp
@@ -11,6 +11,13 @@ int main(int argc, char*argv[])
 	const int t1=6,t2=7;
 	int matri[t1][t2]={0};
 
+	for (int i = 0; i < 7; ++i)
+	{
+		cout << i << "\t";
+	}
+	cout << endl;
+	cout << "*************************************************" << endl;
+
 	for (int i = 0; i < 6; ++i)
 	{
 		for (int j = 0; j < 7; ++j)
@@ -18,6 +25,47 @@ int main(int argc, char*argv[])
 			cout << matri[i][j] << "\t";
 		}
 		cout << endl;
+	}
+
+
+	bool gane = false;
+	while(!gane){
+		int col = -1;
+		while(col<0 || col>6){
+			cout << "Jugada invalida";
+			cout << "Turno del jugador 1.\nIngrese en que columna desea que caiga su ficha: " << endl;
+			cin >> col;
+			if (col<0 || col>6)
+			{
+				cout << "Jugada invalida" << endl;
+			}
+		}
+		//Juga
+		for (int i = 5; i >= 0; --i)
+		{
+			if (matri[i][col]==0)
+			{
+				matri[i][col]=1;
+				i=-1;
+			}
+		}
+
+		//Imprime
+		for (int i = 0; i < 7; ++i)
+		{
+			cout << i << "\t";
+		}
+		cout << endl;
+		cout << "*************************************************" << endl;
+
+		for (int i = 0; i < 6; ++i)
+		{
+			for (int j = 0; j < 7; ++j)
+			{
+				cout << matri[i][j] << "\t";
+			}
+			cout << endl;
+		}
 	}
 	
 	return 0;
